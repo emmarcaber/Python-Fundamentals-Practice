ac16168d4b49a742748ba80d1f2d7b0a082f9d23
Emracabe <caberemmar@gmail.com>
2023-01-09 13:08:23 +0800

    Added the variables tutorial

---
+++ Variables.py
@@ -0,0 +1,27 @@
+# Variable = a container for a value. Behaves as the value that it contains 
+
+# string data type is a series of characters
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
+
+# print(type(first_name)) # to know the type of the variable
+
+# integer data type is for whole numbers
+age = 21
+age += 1
+print("Your age is " + str(age)) # str() is an example of method to use in typecasting
+# print(type(age)) # to know the type of the variable
+
+# float data type is for numbers with decimal values
+height = 250.5
+print("Your height is " + str(height) + " cm")
+# print(type(height)) # to know the type of the variable
+
+# boolean data type is a variable to store True or False
+# useful in conditional statements e.g if, else-if
+human = True
+print("Are you a human? " + str(human))
+# print(type(human)) # to know the type of the variable
