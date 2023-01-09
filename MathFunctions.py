803374534f1e4a669ead6241e62df370b730db45
Emracabe <caberemmar@gmail.com>
2023-01-09 14:47:54 +0800

    Added the MathFunctions Tutorial

---
+++ MathFunctions.py
@@ -0,0 +1,17 @@
+# Import the math library
+import math
+
+# Declare some variables to manipulate 
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+# print(round(pi)) # rounds to nearest integer
+# print(math.ceil(pi)) # rounds up to nearest integer
+# print(math.floor(pi)) # rounds down to nearest integer
+# print(abs(pi)) # gets the absolute value
+# print(pow(pi, 2)) # pow(base, exponent)
+# print(math.sqrt(420)) # gets the square root of a number
+print(max(x, y, z)) # gets the maximum value
+print(min(x, y, z)) # gets the minimum value
