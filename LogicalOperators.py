de7153a48de79ddf0393cdad8808ac7e02770a4b
Emracabe <caberemmar@gmail.com>
2023-01-09 17:50:21 +0800

    Added the explanation of NOT operators in Logical Operators Tutorial

--- LogicalOperators.py
+++ LogicalOperators.py
@@ -7,11 +7,27 @@ temp = int(input("What is the temperature outside? "))
 # AND OPERATORS
 # returns true if both of the conditions are true
 if temp >= 0 and temp <= 30:
-    print("The temperature is good today!")
-    print("Go outside!")
+    print("The temperature is bad today!")
+    print("Stay inside!")
 
 # OR OPERATORS
 # returns true if one of the conditions are true
 elif temp < 0 or temp > 30:
-    print("The temperature is bad today!")
-    print("Stay inside!")
+    print("The temperature is good today!")
+    print("Go outside!")
+
+# NOT OPERATORS
+# reverses the conditioanl statement
+# FOR EXAMPLE:
+"""
+    x = True
+    y = False
+
+    # Return false since x is True
+    result = not x
+    print(result)
+
+    # Return true since y is False
+    result = not y
+    print(result)
+"""
