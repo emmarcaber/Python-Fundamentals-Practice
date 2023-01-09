e2fecc65efb71550af76445a6a06bb33db6aac4e
Emracabe <caberemmar@gmail.com>
2023-01-09 16:58:32 +0800

    Modified StringMethods Tutorial

--- StringMethods.py
+++ StringMethods.py
@@ -1,13 +1,13 @@
-name = "Bro"
+name = "Emmar"
 
 print(len(name)) # len() -> length of the string
-print(name.find("o")) # find() -> find the index of the argument
+print(name.find("m")) # find() -> find the index of the argument
 print(name.capitalize()) # capitalize() -> capitalizes the string
 print(name.upper()) # upper() -> uppercases the string
 print(name.lower()) # lower() -> lowercases the string
 print(name.isdigit()) # isdigit() -> checks if the string is a digit
 print(name.islower()) # islower() -> checks if the string is all in lowercase
 print(name.isalpha()) # isalpha() -> checks if the string is all alphabetic
-print(name.count("o")) # count() -> counts the number of characters in the string
-print(name.replace("o", "a")) # replace() -> replaces the characters in the string
+print(name.count("m")) # count() -> counts the number of characters in the string
+print(name.replace("m", "p")) # replace() -> replaces the characters in the string
 print(str(name + " ") * 3) # multiplies the string by 3 times
