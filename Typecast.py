205428ec3086e6185d96da75e249a8205ff6f81b
Emracabe <caberemmar@gmail.com>
2023-01-09 14:09:27 +0800

    Added the Typecast Practice

---
+++ Typecast.py
@@ -0,0 +1,16 @@
+# type casting = convert the data type of a value to another data type
+
+x = 1 # int
+y = 2.0 # float
+z = "3" # str
+
+x = str(x) # from integer to string 
+y = str(y) # from float to string
+z = int(z) # from string to integer
+
+w = str(z) # from integer to string
+
+print(x)
+print(y)
+print(z * 3)
+print(w * 3)
