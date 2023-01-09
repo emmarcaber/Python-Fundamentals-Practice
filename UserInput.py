a379958ab4e9e6512d3324a443e81e3c7aa82780
Emracabe <caberemmar@gmail.com>
2023-01-09 14:22:30 +0800

    Added the UserInput practice

---
+++ UserInput.py
@@ -0,0 +1,16 @@
+# asks the user for his/her name and stores it in the name variable
+# with a string data type
+name = input("What is your name? ")
+
+# asks the user for his/her age and stores it in the age variable
+# with an int data type
+age = int(input("How old are you? "))
+
+# asks the user for his/her height and stores it in the name variable
+# with a float data type
+height = float(input("How tall are you? "))
+
+# Printing the inputs
+print("Hello, " + name + "!")
+print("You are " + str(age) + " years old.")
+print("You are " + str(height) + " meters tall.")
