f63934206e7cdbcf58dbedea4a698be0222cebe1
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-10 07:04:34 +0000

    Added the While Loop Practice

---
+++ WhileLoop.py
@@ -0,0 +1,19 @@
+# while loop = a statement that will execute it's block of code,
+#              as long as it's condition remains true
+
+# this will result to an infinite loop,
+# since the condition is always true
+# while 1==1:
+#    name = print("Help! I'm stuck in a loop!")
+
+name = ""
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+# NOTE: You can also conditional operators in a while loop
+# while not name:
+# 	name = input("Enter your name: ")
+# It is the same on the example above
+
+print("Hello " + name)  
