7887bb169c5ee810c6e495ed72253c59e226e45a
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-10 08:33:17 +0000

    Created NestedLoops.py

---
+++ NestedLoops.py
@@ -0,0 +1,28 @@
+# nested loops =		The "inner loop" will finish all of it's iterations before
+#						finishing one iteration of the "outer loop"
+
+rows = int(input("How many rows? "))
+columns = int(input("How many columns? "))
+symbol = input("Enter a symbol to use: ")
+
+# outer loop is for rows,
+# whereas inner loop is for columns
+for i in range(rows):
+	for j in range(columns):
+		print(symbol, end="")
+	print()
+
+# another common example for nested loops,
+# is the display some asterisks like this:
+#
+# 	*
+# 	* *
+# 	* * *
+# 	* * * *
+# 	* * * * *
+#
+# You can solve this using the code below:
+# for i in range(6):
+#	for j in range(i):
+#		print("*", end="")
+#	print()
