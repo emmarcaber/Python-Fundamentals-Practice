c40163e2c4988c9ae55f75e1994e5f4bab224a9c
Emracabe <caberemmar@gmail.com>
2023-01-11 13:15:33 +0800

    Created 2DLists.py

---
+++ 2DLists.py
@@ -0,0 +1,26 @@
+# 2D lists = a list of lists
+# also known as multi-dimensional lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+desserts = ["cake", "ice cream"] 
+
+foods = [drinks, dinner, desserts]
+
+# show all the elements of the list
+# prints [['coffee', 'soda', 'tea'], ['pizza', 'hamburger', 'hotdog'], ['cake', 'ice cream']]
+print(foods) 
+
+# show the first list element of foods which is the drinks
+# prints [['coffee', 'soda', 'tea']
+print(foods[0])
+
+# show the first element of drinks which is "coffee"
+print(foods[0][0])
+
+# show the third element of dinner which is "hotdog"
+print(foods[1][2])
+
+# this is an IndexErro
+# since there are only two elements inside the desserts list
+# print(foods[2][2])
