0a9596c2069645f62dd6545a10f3b86aa2f226b1
Emracabe <caberemmar@gmail.com>
2023-01-11 15:08:37 +0800

    Created IndexOperator.py

---
+++ IndexOperator.py
@@ -0,0 +1,17 @@
+# index operator [] = gives access to a sequence's element (str, list, tuples)
+
+name = "bro Code!"
+
+# if name[0].islower():
+#     name = name.capitalize()
+
+# slicing particular elements in the string
+first_name = name[:3].upper()
+last_name = name[4:].lower()
+
+# getting the last character of the string
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
