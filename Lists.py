353d0fc7cfc37f673080e28ea14fdc1c1f9cd740
Emracabe <caberemmar@gmail.com>
2023-01-11 13:05:00 +0800

    Created Lists.py

---
+++ Lists.py
@@ -0,0 +1,16 @@
+# list = used to store multiple items in a single variable
+
+foods = ["pizza", "hamburger", "hotdog", "spaggheti", "pudding"]
+
+foods[0] = "sushi"
+
+# foods.append("ice cream") # add "ice cream" to foods list
+# foods.remove("hotdog") # remove "hotdogs" from foods list
+# foods.pop() # removes the last element
+# foods.insert(0, "cake") # inserts cake in the index 0 of foods list
+# foods.sort() # sorts the foods list in alphabetical order
+# foods.clear() # removes all the items in the foods list
+# foods.reverse() # reverses the orders of items in the foods list
+
+for x in foods:
+    print(x)
