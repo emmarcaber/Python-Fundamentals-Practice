c6327e057a7a6e151763296875ce24c446d8beb1
Emracabe <caberemmar@gmail.com>
2023-01-11 14:18:06 +0800

    Created Sets.py

---
+++ Sets.py
@@ -0,0 +1,21 @@
+# set = collection which is underored, undindexed. 
+#       No duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+# utensils.add("napkin") # adds "napkin" to utensils set
+# utensils.remove("fork") # removes "fork" from utensils set
+# utensils.clear() # removes all the elements inside the utensils set
+# utensils.update(dishes) # concatenates dishes set to utensils set
+# dinner_table = utensils.union(dishes) # shows the union of the sets
+
+# shows the the difference between the sets utensils and dishes
+print(dishes.difference(utensils))
+
+# shows the similarities and intersections between the sets
+# utensils and dishes
+print(dishes.intersection(utensils))
+
+# for x in dinner_table:
+#     print(x)
