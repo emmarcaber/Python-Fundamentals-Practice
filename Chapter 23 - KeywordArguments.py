41cf42acffa4b1780c32fbc2863ff87ee3c8abbc
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-16 18:10:52 +0800

    Update Chapter 23 - KeywordArguments.py

--- Chapter 23 - KeywordArguments.py
+++ Chapter 23 - KeywordArguments.py
@@ -1,6 +1,6 @@
 # keyword arguments =		arguments preceded by an identifier when we pass them to a function.
-#								The order of the arguments doesn't matter, unlike positional arguments
-#								Python knows the names of the arguments that our function receives
+#				The order of the arguments doesn't matter, unlike positional arguments
+#				Python knows the names of the arguments that our function receives
 
 def hello(first, middle, last):
 	print("Hello " + first + " " + middle + " " + last)
