28be5f45cf1f3d0b7b195e3104dbfc0298c29d81
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-16 18:45:09 +0800

    Update Chapter 25 - VariableScopes.py

--- Chapter 25 - VariableScopes.py
+++ Chapter 25 - VariableScopes.py
@@ -1,6 +1,6 @@
 # scope =	The region that a variable is recognized
-#			    A variable is only available from inside the region it is created.
-#			    A global and locally scoped versions of a variable can be created
+#		A variable is only available from inside the region it is created.
+#		A global and locally scoped versions of a variable can be created
 
 # NOTE: Remember the LEGB
 # L = Local
