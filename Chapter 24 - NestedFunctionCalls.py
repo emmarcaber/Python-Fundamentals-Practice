fff5fdb1326b6355bc3177ffa329e60046fa9f45
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-16 18:21:51 +0800

    Create Chapter 24 - NestedFunctionCalls.py

---
+++ Chapter 24 - NestedFunctionCalls.py
@@ -0,0 +1,17 @@
+# nested function calls = 		function calls inside other functioncalls
+# 								            innermost function calls are resolved first
+#								              returned value is used as argument for the next outer function
+
+# No Nested Function Calls
+# num = input("Enter a whole positive number: ")
+# num = float(num)
+# num = abs(num)
+# num = round(num)
+# print(num) 
+
+# Demo of Nested Function Calls
+# First, it will ask about the input
+# Then, convert to a float data type
+# Next, apply the absolute value
+# Now, round the result and print it 
+print(round(abs(float(input("Enter a whole positive number: ")))))
