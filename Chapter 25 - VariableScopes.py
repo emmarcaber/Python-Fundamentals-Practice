b2e3331a7e846c419d3007f11e48203ea842cc51
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-16 18:44:33 +0800

    Create Chapter 25 - VariableScopes.py

---
+++ Chapter 25 - VariableScopes.py
@@ -0,0 +1,19 @@
+# scope =	The region that a variable is recognized
+#			    A variable is only available from inside the region it is created.
+#			    A global and locally scoped versions of a variable can be created
+
+# NOTE: Remember the LEGB
+# L = Local
+# E = Enclosing
+# G = Global
+# B = Built-in
+# Order of Execution
+
+name = "Bro" 			# global scope(available inside & outside functions)
+
+def display_name():
+	name = "Code"	# local scope (available only inside this function)
+	print(name)
+
+display_name()
+print(name)
