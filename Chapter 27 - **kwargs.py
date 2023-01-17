027a5156badf660146ac1cd207c5a511b9a1e037
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-17 15:47:51 +0800

    Create Chapter 27 - **kwargs.py

---
+++ Chapter 27 - **kwargs.py
@@ -0,0 +1,10 @@
+# **kwargs =	parameter that will pack all arguments into a dictionary
+#				      useful so that a function can accept a varying aount of keyword arguments
+
+def hello(**kwargs):
+	#print("Hello " + kwargs['first'] + " " + kwargs['last'])
+	print("Hello", end=" ")
+	for key, value in kwargs.items():
+		print(value, end=" ")
+
+hello(title="Mr.", first="Embre", middle="Emra",last="Cabe")
