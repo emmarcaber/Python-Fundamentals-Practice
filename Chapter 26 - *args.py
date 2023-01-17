e926b2c27e524f30d7296cd74b3c86febee774b3
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-17 15:38:09 +0800

    Create Chapter 26 - *args.py

---
+++ Chapter 26 - *args.py
@@ -0,0 +1,12 @@
+# *args = 	parameter that will pack all arguments into a tuple
+#			      useful so that a function can accept a varying amount of arguments
+
+def add(*args):
+	sum = 0
+	args = list(args) 	# convert it to a list, so that you can reassign some values
+	args[0] = 0		# assigning index 0 of stuff list equals to 0
+	for i in args:
+		sum += i
+	return sum
+
+print(add(1,2,3, 4, 5, 6))
