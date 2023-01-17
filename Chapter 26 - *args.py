4f7f998e07e90fac26ef2462b8d1db339da1220d
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-17 15:48:12 +0800

    Update Chapter 26 - *args.py

--- Chapter 26 - *args.py
+++ Chapter 26 - *args.py
@@ -1,5 +1,5 @@
 # *args = 	parameter that will pack all arguments into a tuple
-#			      useful so that a function can accept a varying amount of arguments
+#		useful so that a function can accept a varying amount of arguments
 
 def add(*args):
 	sum = 0
