b01f8a7eb7f720ae03c132caf073de0e53dc8e1c
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-01-17 16:45:09 +0800

    Create Chapter 29 - RandomModule.py

---
+++ Chapter 29 - RandomModule.py
@@ -0,0 +1,16 @@
+import random
+
+x = random.randint(1, 6) 		# generate a random number from 1 to 6
+y = random.random()			# generate a random floating number between 0 and 1
+
+myList = ['rock', 'paper', 'scissors']
+z = random.choice(myList) 	# generate a random choice from the list
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+random.shuffle(cards)		# shuffle the cards randomly
+
+print(x)
+print(y)
+print(z)
+print(cards)
