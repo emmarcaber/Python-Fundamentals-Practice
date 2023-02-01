230bdf8696d68efd7e2fc2fb30ad62564f9f3798
Emracabe <caberemmar@gmail.com>
2023-02-01 13:30:14 +0800

    Create Chapter 36 - DeleteFile.py

---
+++ Chapter 36 - DeleteFile.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = 'test.txt'
+
+try:
+    os.remove(path) # delete a file
+    # os.rmdir(path) # delete a file or empty folder
+    # shutil.rmtree(path) # delete files and/or folders
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to delete that")    
+except:
+    print("That folder contains files")
+else:
+    print(path + " was deleted")
