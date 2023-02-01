642f3c2e94c505ceaff993601d74c2d990521ac5
Emracabe <caberemmar@gmail.com>
2023-02-01 13:35:38 +0800

    Create Chapter 37 - Modules.py

---
+++ Chapter 37 - Modules.py
@@ -0,0 +1,9 @@
+# module = a file containiong python code. May contain functions, classes, etc.
+# Used with modular programming, which is to separate a program into parts
+
+# import messages
+# import messages as m
+# from messages import hello, bye
+# from messsages import * (this one is dangerous)
+
+help("modules")
