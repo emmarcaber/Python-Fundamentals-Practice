7118d05ff71efc32e3cc5f5326f07ae8984caecf
Emracabe <caberemmar@gmail.com>
2023-02-01 14:19:12 +0800

    Update Chapter 39 - QuizGame.py

--- Chapter 39 - QuizGame.py
+++ Chapter 39 - QuizGame.py
@@ -14,3 +14,10 @@ def display_score():
 # -------------------------------
 def play_again():
     pass
+
+questions = {
+    "Who created Python? " : "A",
+    "What year was Python created? " : "B",
+    "Python is a tribute to which comedy group? " : "C",
+    "Is the Earth round? " : "A"
+}
