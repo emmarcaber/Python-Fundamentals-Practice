9b70483ba15e639fbf8a00c4ff159917167600e5
Emracabe <caberemmar@gmail.com>
2023-02-01 13:16:45 +0800

    Create Chapter 34 - CopyFile.py and copy.txt

---
+++ Chapter 34 - CopyFile.py
@@ -0,0 +1,11 @@
+# copyfile() = copies contents of a file
+# copy()     = copyfile() + permission mode + destination can be a directory
+# copy2()    = copy() + copies metadata (file's creation and modification times)
+
+# Module to copy files
+import shutil
+
+# Copy the contests of test.txt to copy.txt
+shutil.copyfile('test.txt', 'copy.txt')
+
+# shutil.copyfile(source, destination)
