c7847fd18f3296afa5d8e46c57f87d3d0566d265
Emracabe <caberemmar@gmail.com>
2023-02-01 14:22:00 +0800

    Update Chapter 39 - QuizGame.py

--- Chapter 39 - QuizGame.py
+++ Chapter 39 - QuizGame.py
@@ -21,3 +21,10 @@ questions = {
     "Python is a tribute to which comedy group? " : "C",
     "Is the Earth round? " : "A"
 }
+
+options = [
+    ["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerberg"],
+    ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+    ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+    ["A. True", "B. False", "C. Sometimes", "D. What is Earth?"]
+]
