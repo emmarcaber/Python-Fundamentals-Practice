9d532e0fc0cd796d0ed97ab9ff01fc0219c6e976
Emracabe <caberemmar@gmail.com>
2023-02-01 14:08:32 +0800

    Create Chapter 38 - RockPaperScissors.py

---
+++ Chapter 38 - RockPaperScissors.py
@@ -0,0 +1,49 @@
+import random
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock, paper, scissors? ").lower()
+
+    if player == computer:
+        print(f"computer: {computer}")
+        print(f"player: {player}")
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You lose!")
+        elif computer == "scissors":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You win!")
+    elif player == "paper":
+        if computer == "scissors":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You lose!")
+        elif computer == "rock":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You win!")
+    elif player == "scissors":
+        if computer == "rock":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You lose!")
+        elif computer == "paper":
+            print(f"computer: {computer}")
+            print(f"player: {player}")
+            print("You win!")
+
+    play_again = input("Play again? (yes/no)? ").lower()
+
+    if play_again != "yes":
+        break
+
+print("Bye!")
