1469f568796062f4c68d58a3ea10a50609c00f8a
Emracabe <caberemmar@gmail.com>
2023-02-01 13:23:52 +0800

    Create Chapter 35 - MoveFile.py

---
+++ Chapter 35 - MoveFile.py
@@ -0,0 +1,21 @@
+import os
+
+source = "test.txt"
+destination = "C:\\Users\\CSPC CCS\\test.txt"
+
+try:
+    
+    # Check if the path exists
+    if os.path.exists(destination):
+        print("There is already a file there")
+    
+    # Else, if there is no path existing
+    # Move the file to the destination
+    else:
+        os.replace(source, destination)
+        print(source + " was moved")
+
+# If there is no file to move
+# catch the exception
+except FileNotFoundError:
+    print(source + " was not found")
