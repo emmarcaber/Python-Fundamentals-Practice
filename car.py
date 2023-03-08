abad5e83e2e18e9866d860fd008077a00a1b299b
Emmar Caber <caberemmar@gmail.com>
2023-03-08 22:09:35 +0800

    Update car.py, added some comments

--- car.py
+++ car.py
@@ -6,14 +6,21 @@ class Car:
 	# year = None
 	# color = None
 	
+	# An example of class variable
+	# A variable that solely belongs to the class
+	wheels = 4
+
 	# This is a special method
 	# It is known as constructor in other
 	# programming languages
 	def __init__(self, make, model, year, color):
-		self.make = make
-		self.model = model
-		self.year = year
-		self.color = color
+		self.make = make 				# instance variable
+		self.model = model				# instance variable
+		self.year = year				# instance variable
+		self.color = color				# instance variable
+
+	# NOTE: Instance variables are variables 
+	# 		that will be changed for a specific object
 		
 	# NOTE: You don't need to pass an argument 
 	# 		  in the self parameter
