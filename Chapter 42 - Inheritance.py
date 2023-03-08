62dad1e70ad056ef16b2aba2cc805fb812d4735c
Emmar Caber <caberemmar@gmail.com>
2023-03-09 00:24:17 +0800

    Update Chapter 42 - Inheritance.py

--- Chapter 42 - Inheritance.py
+++ Chapter 42 - Inheritance.py
@@ -1,3 +1,10 @@
+# NOTE: Inheritance allows developers
+#       to not copy and paste code because
+#       of changing multiple classes that 
+#       can be clustered or grouped
+
+
+# Parent Class
 class Animal:
     
     alive = True
@@ -9,15 +16,49 @@ class Animal:
         print("This animal is sleeping")
 
 
+# Subclass
+# Rabbit inherits the properties and methods
+# of Animal class
+# Meaning rabbit can call eat() and sleep()
+# from the Animal Cass 
 class Rabbit(Animal):
-    pass
+    def run(self):
+        print("This rabbit is running")
+
+
+
+# Subclass
+# Fish inherits the properties and methods
+# of Animal class
+# Meaning rabbit can call eat() and sleep()
+# from the Animal Cass 
 class Fish(Animal):
-    pass
+    def swim(self):
+        print("This fish is sleeping")
+
+
+# Subclass
+# Hawk inherits the properties and methods
+# of Animal class
+# Meaning rabbit can call eat() and sleep()
+# from the Animal Cass 
 class Hawk(Animal):
-    pass
+    def fly(self):
+        print("This hawk is flying")
 
 rabbit = Rabbit()
 fish = Fish()
-Hawk = Hawk()
+hawk = Hawk()
 
-print(rabbit.alive)
+# Calling the properties or method
+# from the Parent Class
+# Demonstrating the inheritance
+# print(rabbit.alive)
+# fish.eat()
+# hawk.sleep()
+
+# Calling the methods unique
+# to each subclass
+rabbit.run()
+fish.swim()
+hawk.fly()
