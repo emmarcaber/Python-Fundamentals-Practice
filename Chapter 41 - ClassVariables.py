4a4daaf1b9217773b969b7ef01023489ea950347
Emmar Caber <caberemmar@gmail.com>
2023-03-08 22:13:21 +0800

    Update Chapter 41 - ClassVariables.py and .gitignore

--- Chapter 41 - ClassVariables.py
+++ Chapter 41 - ClassVariables.py
@@ -1,1 +1,12 @@
 from car import Car
+
+car1 = Car("Chevy", "Corvette", 2021, "blue")
+car2 = Car("Ford", "Mustang", 2022, "red")
+
+# Changes all the value of wheels variables to 2
+# Since the wheels belongs to the class
+# All objects of Car classes will be affected
+Car.wheels = 2
+
+print(car1.wheels)
+print(car2.wheels)
