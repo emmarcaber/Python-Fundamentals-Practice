02b2abd51137e5f0bdebfc0de541956081a921cd
Emmar Caber <caberemmar@gmail.com>
2023-03-11 11:37:14 +0800

    Update Chapter 45 - MethodOverriding.py

--- Chapter 45 - MethodOverriding.py
+++ Chapter 45 - MethodOverriding.py
@@ -1,2 +1,22 @@
 # CHAPTER 45
 # Method Overriding
+
+class Animal:
+
+    def eat(self):
+        print("This animal is eating")
+
+# Method Overriding
+class Rabbit(Animal):
+    
+    # Method which overrides the eat() method
+    # of Animal class
+    def eat(self):
+        print("This rabbit is eating")
+
+
+rabbit = Rabbit()
+
+# Prints "This rabbit is eating"
+# Instead of, "This animal is eating"
+rabbit.eat()
