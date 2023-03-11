0f6da788f2fcafc35981ca660597536096153b2f
Emmar Caber <caberemmar@gmail.com>
2023-03-11 11:02:16 +0800

    Update Chapter 43 - MultilevelInheritance.py

--- Chapter 43 - MultilevelInheritance.py
+++ Chapter 43 - MultilevelInheritance.py
@@ -1,1 +1,26 @@
 # CHAPTER 43
+# Multilevel Inheritance = when a derived (child) class inherits another derived (child) class 
+
+# Super Parent Class
+class Organism:
+    alive = True
+
+# Parent Class of Dog
+# but a Child class of Animal
+class Animal(Organism):
+
+    def eat(self):
+        print("This animal is eating")
+
+# Child class of Animal
+class Dog(Animal):
+
+    def bark(self):
+        print("This dog is barking")
+
+
+# Testing the inheritance
+dog = Dog()
+print(dog.alive)
+dog.eat()
+dog.bark()
