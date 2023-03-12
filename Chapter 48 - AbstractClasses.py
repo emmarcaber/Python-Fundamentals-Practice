c1086f90a5e10b2ae43466a1f4c32ce85f40b156
Emracabe <caberemmar@gmail.com>
2023-03-12 21:21:25 +0800

    Update Chapter 48 - AbstractClasses.py

--- Chapter 48 - AbstractClasses.py
+++ Chapter 48 - AbstractClasses.py
@@ -1,2 +1,9 @@
 # CHAPTER 48
 # Abstract Classes
+
+# Prevents a user from creating an object of that class
+# + compels a user to override abstract methods in a child class
+
+# Abstract Class = a class which contains one or more abstract methods
+# Abstract Method = a method that has a declaration but does not have an implementation
+
