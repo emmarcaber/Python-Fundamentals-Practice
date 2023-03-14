f6edea27ea2d283d1446bdc1e88b0e8b0f895706
Emmar Caber <71781006+emmarcaber@users.noreply.github.com>
2023-03-14 13:29:47 +0800

    Update Chapter 49 - ObjectsAsArguments.py

--- Chapter 49 - ObjectsAsArguments.py
+++ Chapter 49 - ObjectsAsArguments.py
@@ -1,2 +1,25 @@
 # CHAPTER 49
 # Objects as Arguments
+
+class Car:
+
+    color = None
+
+# Function with car objects as arguments
+def change_car_color(car, color):
+
+    # Changes the color of the object argument
+    car.color = color
+
+car_1 = Car()  
+car_2 = Car()
+car_3 = Car()
+
+# Passing the different car objects as arguments
+change_car_color(car_1, "red")
+change_car_color(car_2, "green")
+change_car_color(car_3, "blue")
+
+print(car_1.color)
+print(car_2.color)
+print(car_3.color)
