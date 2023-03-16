6ce46a520e807420648f14f7eb255beb5bef62b9
Emmar Caber <caberemmar@gmail.com>
2023-03-17 00:39:46 +0800

    Update Chapter 54 - Lambda.py

--- Chapter 54 - Lambda.py
+++ Chapter 54 - Lambda.py
@@ -1,2 +1,7 @@
 # CHAPTER 54
-# Lambda Function
+# Lambda Function = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression
+#                   (think of it as a shortcut)
+#                   (useful if needed for a short period of time, throw-away)
+
+# lambda parameters : expression
