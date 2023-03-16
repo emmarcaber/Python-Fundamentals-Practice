62b18acf2b360a82505236fd41738ca17a25689b
Emmar Caber <caberemmar@gmail.com>
2023-03-17 00:08:25 +0800

    Update Chapter 53 - HigherOrderFunctions.py

--- Chapter 53 - HigherOrderFunctions.py
+++ Chapter 53 - HigherOrderFunctions.py
@@ -1,2 +1,6 @@
 # CHAPTER 53
 # Higher Order Functions
+# A function that either:
+#   1. Accepts a function as argument
+#   2. Returns a function
+#   (In Python, functions are also treated as objects)
