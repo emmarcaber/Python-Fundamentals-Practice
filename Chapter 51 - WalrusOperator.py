c1d0b1fc4bc6fcb90d34e34e34760e4d47262c5b
Emmar Caber <caberemmar@gmail.com>
2023-03-16 20:45:05 +0800

    Update Chapter 51 - WalrusOperator.py

--- Chapter 51 - WalrusOperator.py
+++ Chapter 51 - WalrusOperator.py
@@ -1,2 +1,26 @@
 # CHAPTER 51
 # Walrus Operator :=
+# Assignement Expression a.k.a Walrus Operator
+# Assign Values to Variable as Part of Larger Expression
+
+
+# TO understand better the Walrus Operator
+# Just remember to evaluate first the expression
+# Then assign the result to the variable
+
+# foods = list()
+# while True:
+#     food = input("What food do you like? ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+
+foods = list()
+while food := input("What food do you like? ")  != "quit":
+    foods.append(food)
+
+print(foods)
+
+print(happy := True)
+print(happy)
