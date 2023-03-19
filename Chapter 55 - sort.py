1288741fabe26b1614627739de5720e025527e4b
Emracabe <caberemmar@gmail.com>
2023-03-19 20:37:00 +0800

    Chapter 55 - sort.py

--- Chapter 55 - sort.py
+++ Chapter 55 - sort.py
@@ -3,3 +3,10 @@
 
 # sort() = sort with lists
 # sorted() = sort with iterables(tuples, dictionary, sets)
+
+students = ["Embre", "ABEN", "Meracabe", "Hakdog"]
+
+# sort the students list
+students.sort()
+
+print(students)
