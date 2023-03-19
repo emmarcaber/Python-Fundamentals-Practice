3248fc1234b53d13a3743eaf09bbd4987a858c18
Emracabe <caberemmar@gmail.com>
2023-03-20 07:28:37 +0800

    Update Chapter 56 - map.py

--- Chapter 56 - map.py
+++ Chapter 56 - map.py
@@ -2,3 +2,19 @@
 # map() = applies a function to each item in an iterable (list, tuple, etc)
 # map(function, iterables)
 
+store = [
+    ("shirt", 20.00),
+    ("pants", 25.00),
+    ("jacket", 50.00),
+    ("socks", 10.00),
+]
+
+# Get all the tuple element in the store list
+# Convert the second element which is price into euro
+to_euros = list(map(lambda data : (data[0], data[1] * 0.82), store))
+
+to_dollars = list(map(lambda data: (data[0], round(data[1] / 0.82, 2)), store))
+
+print(*to_euros, sep="\n")
+print()
+print(*to_dollars, sep="\n")
