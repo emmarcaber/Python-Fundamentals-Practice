00b0461e1b731cfa86dbf7b496c7bc002bcf6e93
Emmar Caber <caberemmar@gmail.com>
2023-03-22 13:19:15 +0800

    Update Chapter 59 - ListComprehension.py

--- Chapter 59 - ListComprehension.py
+++ Chapter 59 - ListComprehension.py
@@ -4,3 +4,18 @@
 #                      list = [expression for item in iterable]
 #                      list = [expression for item in iterable if conditional]
 #                      list = [expression if/else for item in iterable]
+
+students = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+
+# With list and filter
+passed_students = list(filter(lambda x : x >= 60, students))
+print(passed_students)
+
+# Result is Like the method above, but with list comprehension
+passed_students = [i for i in students if i >= 60]
+print(passed_students)
+
+# IF i is greater than or equal to 60, append the grade as a number
+# ELSE append "FAILED" in the passed_students list
+passed_students = [i if i >= 60 else "FAILED" for i in students]
+print(passed_students)
