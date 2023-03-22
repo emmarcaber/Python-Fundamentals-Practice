af8e00296372a50122d0d824c5a2b23f00fec3fb
Emmar Caber <caberemmar@gmail.com>
2023-03-22 13:13:11 +0800

    Update Chapter 59 - ListComprehension.py

--- Chapter 59 - ListComprehension.py
+++ Chapter 59 - ListComprehension.py
@@ -1,1 +1,6 @@
 # CHAPTER 59
+# List Comprehension = a way to create a new list with less syntax
+#                      can mimic certain lambda functions, easier to read
+#                      list = [expression for item in iterable]
+#                      list = [expression for item in iterable if conditional]
+#                      list = [expression if/else for item in iterable]
