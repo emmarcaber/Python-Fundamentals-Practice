ef89082f9386ab485dc1cec26658ab086fec241d
Emmar Caber <caberemmar@gmail.com>
2023-04-03 17:55:45 +0800

    Create Chapter 62 if__name__=='__main__'.py

---
+++ Chapter 62 if__name__=='__main__'.py
@@ -0,0 +1,10 @@
+# CHAPTER 62
+# if __name__ == '__main__'
+
+# y tho?
+# 1. Module can be run as standalone program
+# 2. Module can be imported and used by other modules
+
+# Python interpreter sets "special variables", one of which is __name__
+# then Python will execute the code and found within __main__
+
